be04de6036e8408a8001c2b797fbecad5f7c8832
Ali Aljendy <alialjndy2@gmail.com>
2025-05-14 17:50:13 +0300

    Add-feat | add helper method response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,18 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    protected function success(array $data = [],$code = 200,$message = 'Done Successfully!',$status = 'success'){
+        return response()->json([
+            'status'=>$status,
+            'message'=>$message,
+            'data'=>$data
+        ],$code);
+    }
+    protected function error($message = 'Error Occurred',$status='error',$code = 400,array $errors = []){
+        return response()->json([
+            'status'=>$status ,
+            'message'=>$message,
+            'errors'=>$errors ?: null ,
+        ],$code);
+    }
 }
