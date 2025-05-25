3172a18a1b0d3ee7a0175570c72bc4db605d00c2
Ali Aljendy <alialjndy2@gmail.com>
2025-05-25 12:42:10 +0300

    Remove commented-out code

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\EmailVerificationController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\SocialAuthController;
 
@@ -11,8 +12,3 @@ Route::controller(SocialAuthController::class)->group(function () {
     Route::get('/auth/google/redirect', 'redirectToGoogle');
     Route::get('/auth/google/callback', 'handleGoogleCallback');
 });
-
-// Route::get('/reset-password/{token}', function ($token) {
-//     return 'This is the reset link page for token: ' . $token;
-// })->name('password.reset');
-
