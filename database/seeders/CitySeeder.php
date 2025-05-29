956053aafc074d2a279da69064057b90c62dde75
Ali Aljendy <alialjndy2@gmail.com>
2025-05-29 12:17:59 +0300

    feat | City : create seeder

---
+++ database/seeders/CitySeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+use App\Models\City;
+use App\Models\Country;
+
+class CitySeeder extends Seeder
+{
+    public function run(): void
+    {
+        $json = File::get(database_path('data/states.json'));
+        $cities = json_decode($json , true);
+        foreach($cities as $city){
+            City::updateOrCreate(
+                ['id' => $city['id']],
+                [
+                    'name' => $city['name'],
+                    'country_id' => $city['country_id'],
+                ]
+            );
+        }
+    }
+}
+
